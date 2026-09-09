eeed769cf1ca664c357d028d11f98cfdc820060f
beagp2703 <beagp2703@gmail.com>
2026-09-09 18:54:20 -0300

    style: adiciona folha de estilos à aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Header from "./components/Header/Header.jsx";
 import Main from "./components/Main/Main.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import "./assets/styles/global.css";
 
 function App() {
   return (
